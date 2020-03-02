ed8b08939dc6af1a9283d7e0bdd7900203c66bbf
loryuen <56411330+loryuen@users.noreply.github.com>
2020-03-02 15:18:12 -0500

    filtered table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -18,5 +18,38 @@ tableData.forEach( (UFOdata) => {
 });
 
 // 3 - Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
+
+// define variables
 var userInput = d3.select('#datetime');
-var filterOutput = d3.select('')
+var filterOutput = d3.select('td');
+var filterButton = d3.select('#filter-btn');
+
+// define filter function
+// function filteredData(data) {
+//     return data.datetime === userInputValue;
+// };
+
+// connect with filter button 
+filterButton.on("click", () => {
+    userInputValue = userInput.property("value");
+
+    console.log(userInputValue);
+    
+    // var filteredData = tableData.filter(data => data.datetime === userInputValue);
+    function filteredData(data) {
+        return data.datetime === userInputValue;
+    };
+
+    var output = tableData.filter(filteredData);
+
+    output.forEach( (UFOdatum) => {
+        var tr = tbody.append('tr');
+        Object.entries(UFOdatum).forEach( ([key, value]) => {
+            var cell = tr.append('td');
+            cell.text(value);
+        });
+    });
+
+   console.log(output);
+
+});
