7d5830d58eebf25b986f64609e2f831f6b4e92fc
loryuen <56411330+loryuen@users.noreply.github.com>
2020-03-03 16:42:44 -0500

    filter if else logic

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,14 +3,15 @@ var tableData = data;
 
 // 1 - define variables
 var tbody = d3.select('tbody');
-var userInputDate = d3.select('#datetime');
 var filterButton = d3.select('#filter-btn');
+var userInputDate = d3.select('#datetime');
+var userInputCity = d3.select('#city');
+var userInputState = d3.select('#state');
+var userInputCountry = d3.select('#country');
+var userInputShape = d3.select('#shape');
 
 
-// 2 - loop through data, 
-//     use d3 to append tr for each data point
-//     use object.entries and for each to iterate through keys and values
-//     append each data point to td tag
+// 2 - loop through data and display
 tableData.forEach( (UFOdata) => {
     var tr = tbody.append('tr');
     Object.entries(UFOdata).forEach( ([key, value]) => {
@@ -19,17 +20,25 @@ tableData.forEach( (UFOdata) => {
     });
 });
 
-// 3 - Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
-
-
-
-// connect with filter button 
+// 3 - List for events and filter upon click
 filterButton.on("click", () => {
-    var userInputDateValue = userInputDate.property("value");
-    console.log(userInputDateValue);
-    console.log(userInputDateValue.length);
+    var userInputDateValue = userInputDate.property('value');
+    var userInputCityValue = userInputCity.property('value');
+    var userInputStateValue = userInputState.property('value');
+    var userInputCountryValue = userInputCountry.property('value');
+    var userInputShapeValue = userInputShape.property('value');
 
-    if (userInputDateValue.length===0) {
+    let filters = [
+        userInputDateValue,
+        userInputCityValue,
+        userInputStateValue,
+        userInputCountryValue,
+        userInputShapeValue
+    ];
+
+    console.log(filters);
+
+    if (filters.join("").length === 0) {
         document.getElementById('tableID').innerHTML='';
         tableData.forEach( (UFOdata) => {
             var tr = tbody.append('tr');
@@ -46,9 +55,14 @@ filterButton.on("click", () => {
 
         // var filteredData = tableData.filter(data => data.datetime === userInputValue);
         function filteredData(data) {
-            return data.datetime === userInputDateValue;
+            return data.shape === userInputShapeValue 
+            // && data.city === userInputCityValue 
+            // && data.state === userInputStateValue 
+            // && data.country === userInputCountryValue
+            // && data.shape === userInputShapeValue
         };
-
+        console.log(userInputDateValue);
+        console.log(tableData.filter(filteredData))
         var output = tableData.filter(filteredData);
 
         output.forEach( (UFOdatum) => {
@@ -61,6 +75,77 @@ filterButton.on("click", () => {
         
         console.log(output);
     }
-
 });
+        
 
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////
+
+// // from data.js
+// var tableData = data;
+
+// // 1 - define variables
+// var tbody = d3.select('tbody');
+// var userInputDate = d3.select('#datetime');
+// var filterButton = d3.select('#filter-btn');
+
+
+// // 2 - loop through data, 
+// //     use d3 to append tr for each data point
+// //     use object.entries and for each to iterate through keys and values
+// //     append each data point to td tag
+// tableData.forEach( (UFOdata) => {
+//     var tr = tbody.append('tr');
+//     Object.entries(UFOdata).forEach( ([key, value]) => {
+//         var cell = tr.append('td');
+//         dataDisplayed = cell.text(value);
+//     });
+// });
+
+// // 3 - Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
+
+
+
+// // connect with filter button 
+// filterButton.on("click", () => {
+//     var userInputDateValue = userInputDate.property("value");
+//     console.log(userInputDateValue);
+//     console.log(userInputDateValue.length);
+
+//     if (userInputDateValue.length===0) {
+//         document.getElementById('tableID').innerHTML='';
+//         tableData.forEach( (UFOdata) => {
+//             var tr = tbody.append('tr');
+//             Object.entries(UFOdata).forEach( ([key, value]) => {
+//                 var cell = tr.append('td');
+//                 dataDisplayed = cell.text(value);
+//             });
+//         });
+//         console.log(userInputDate);
+//     }
+//     else {
+//         // override or clear out current data displayed
+//         document.getElementById('tableID').innerHTML='';
+
+//         // var filteredData = tableData.filter(data => data.datetime === userInputValue);
+//         function filteredData(data) {
+//             return data.datetime === userInputDateValue;
+//         };
+
+//         var output = tableData.filter(filteredData);
+
+//         output.forEach( (UFOdatum) => {
+//             var tr = tbody.append('tr');
+//             Object.entries(UFOdatum).forEach( ([key, value]) => {
+//                 var cell = tr.append('td');
+//                 cell.text(value);
+//             });
+//         });
+//         console.log(output);
+//     }
+// });
